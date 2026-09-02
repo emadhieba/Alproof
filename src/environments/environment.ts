d5e41ad3562089bc7940c084eb2b271ef0bedc77
Emad Heiba <EMADHEBA975@GMAIL.COM>
2026-09-02 15:21:02 +0300

    Fix address to Fayoum

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   bookingEmail: 'your-email@example.com',
   instagram: 'https://instagram.com/profbarbershop',
   facebook: 'https://facebook.com/profbarbershop',
-  address: 'ALPROOF Coffer, Zagazig, Egypt',
+  address: 'ALPROOF Coffer, Fayoum, Egypt',
   mapEmbedUrl:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3452.0!2d30.8405847!3d29.3197814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14597995ffb6b79d%3A0xb437485a4a1aa29a!2sALPROOF%20Coffer!5e0!3m2!1sen!2seg!4v1710000000000!5m2!1sen!2seg',
 };
